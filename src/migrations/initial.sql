599c215a8066c3678ccec7c7c657874accc14cc5
Parris Khachi <parris@letswoosh.com>
2017-07-23 17:33:34 -0700

    Better initial sql

--- src/migrations/initial.sql
+++ src/migrations/initial.sql
@@ -1,3 +1,11 @@
+CREATE SCHEMA postgraphql_watch;
+CREATE FUNCTION notify_watchers() RETURNS event_trigger
+    LANGUAGE plpgsql
+    AS $$ begin perform pg_notify( 'postgraphql_watch', (select array_to_json(array_agg(x)) from (select schema_name as schema, command_tag as command from pg_event_trigger_ddl_commands()) as x)::text ); end; $$;
+CREATE EVENT TRIGGER postgraphql_watch ON ddl_command_end
+         WHEN TAG IN ('ALTER DOMAIN', 'ALTER FOREIGN TABLE', 'ALTER FUNCTION', 'ALTER SCHEMA', 'ALTER TABLE', 'ALTER TYPE', 'ALTER VIEW', 'COMMENT', 'CREATE DOMAIN', 'CREATE FOREIGN TABLE', 'CREATE FUNCTION', 'CREATE SCHEMA', 'CREATE TABLE', 'CREATE TABLE AS', 'CREATE VIEW', 'DROP DOMAIN', 'DROP FOREIGN TABLE', 'DROP FUNCTION', 'DROP SCHEMA', 'DROP TABLE', 'DROP VIEW', 'GRANT', 'REVOKE', 'SELECT INTO')
+   EXECUTE PROCEDURE postgraphql_watch.notify_watchers();
+
 CREATE TABLE person (
     id integer NOT NULL,
     first_name text NOT NULL,
@@ -8,3 +16,12 @@ CREATE TABLE person (
     CONSTRAINT person_last_name_check CHECK ((char_length(last_name) < 80))
 );
 
+CREATE SEQUENCE person_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE person_id_seq OWNED BY person.id;
+ALTER TABLE ONLY person ALTER COLUMN id SET DEFAULT nextval('person_id_seq'::regclass);
